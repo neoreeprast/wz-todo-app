c81c4b549c773f2f9cfc8eb42ecfde041a63414c
Arie <arie.prastowo@gmail.com>
2023-11-12 08:45:25 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  function handleNewTodoChange(event) {
+    setNewTodo(event.target.value);
+  }
+
+  function handleNewTodoSubmit(event) {
+    event.preventDefault();
+    if (newTodo.trim()) {
+      setTodos([...todos, { text: newTodo, done: false }]);
+      setNewTodo('');
+    }
+  }
+
+  function handleTodoDelete(index) {
+    setTodos(todos.filter((_, i) => i !== index));
+  }
+
+  function handleTodoEdit(index, newText) {
+    setTodos(todos.map((todo, i) => i === index ? { ...todo, text: newText } : todo));
+  }
+
+  function handleTodoToggleDone(index) {
+    setTodos(todos.map((todo, i) => i === index ? { ...todo, done: !todo.done } : todo));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <form onSubmit={handleNewTodoSubmit}>
+        <input type="text" value={newTodo} onChange={handleNewTodoChange} placeholder="Add a new todo..." />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.done ? 'done' : ''}>
+            <span>{todo.text}</span>
+            <button onClick={() => handleTodoEdit(index, prompt('Enter new text:', todo.text))}>Edit</button>
+            <button onClick={() => handleTodoDelete(index)}>Delete</button>
+            <button onClick={() => handleTodoToggleDone(index)}>{todo.done ? 'Undo' : 'Done'}</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
